e64a98204192586d41ad52cdcd38ce2de00802f3
Mohammad Mehboudi <43061055+Mehboudi@users.noreply.github.com>
2024-06-10 15:10:46 +0200

    Add files via upload

--- run_factorisation_and_resample.m
+++ run_factorisation_and_resample.m
@@ -3,7 +3,7 @@
 imax=1;
 for i1=1:1:imax
     [i1,imax]
-    Factorisation;
+    Factorisation_new;
     size_resample;
 end
 %% Load and analyze the data
@@ -125,58 +125,3 @@ xlabel('$\omega_m t/\pi$','Interpreter','latex');
 Allan
 % figure
 % histogram(dtj_stable,bin)
-%% Allan Variance
-%% UPDATE: Run "Allan" insterad!
-% avar=cell(1,10);
-% tau=cell(1,10);
-% for ii=1:10
-% imin=ii;
-% imax=ii;
-% dtj=[];
-% dtj_stable=[];
-% for i1=imin:1:imax
-%     myVars = {"tvec_dN1"};
-%     load(['new_data_down_resloved_',num2str(i1)],myVars{:})
-%     %%%%This line will be passed only if you want to filter (detector dead time)
-%     %%%The detector parameters are set on the other code, check it out
-%     %%%there.
-%     if det_filt==1
-%         Detector_Filter;tvec_dN1=tvec_dN1_I2(1:end);
-%     end
-%     %%%%Otherwise carryout as usual
-%     dtjump=[diff([0,tvec_dN1])];sdtj=length(dtjump);
-%     dtjump_stable=dtjump(floor(sdtj/2):end);
-%     dtj=[dtj,dtjump];
-%     dtj_stable=[dtj_stable,dtjump_stable];
-% end
-% [avar{1,ii},tau{1,ii}]=allanvar(dtj','octave',pi);
-% end
-% stau=length(tau{1,1});
-% for ii=2:10
-%     if stau>length(tau{1,ii})
-%         stau=length(tau{1,ii});
-%     end
-% end
-% avarmat=zeros(10,stau);
-% for ii=1:10
-%     dummy=avar{1,ii};
-%     avarmat(ii,:)=dummy(1:stau);
-% end
-% tau=tau{1,1}(1:stau);
-% avarav=mean(avarmat,1);
-% figure
-% hold on
-% loglog(tau*w_m/pi,sqrt(avarmat),'bl:','LineWidth',.1,'HandleVisibility','off')
-% loglog(tau*w_m/pi,sqrt(avarav),'b-','LineWidth',2)
-% xlabel('Averaging Time (\tau\omega_m/\pi)')
-% ylabel('\sigma')
-% title('Allan Deviation')
-% grid on
-% %%%%%%
-% %According to Mitchison's paper, the Allan Variance at long times should
-% %converge to avar-->mu/(NT), where N=mu^2/var. with mu and var being the
-% %usual mean and variance. Let's check this for our signal
-% %mu=mean(dtj);var=std(dtj)^2;
-% avar_asym=mu./(tau*mu^2/var);
-% loglog(tau*w_m/pi,sqrt(avar_asym),'r--','LineWidth',2)
-% legend('$\sigma_A$','$\sigma_{\rm asym}$','interpreter','latex')
